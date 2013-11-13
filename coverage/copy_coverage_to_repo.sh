1b0464e6c17f3ebc22bb5f5d1aff69086b341274
tcbeutler <tcbeutler@gmail.com>
2013-11-12 21:48:32 -0600

    take 5, move coverage results

--- coverage/copy_coverage_to_repo.sh
+++ coverage/copy_coverage_to_repo.sh
@@ -9,8 +9,8 @@ if [ "$TRAVIS_PULL_REQUEST" == "false" ]; then
   git config --global user.email "travis@travis-ci.org"
   git config --global user.name "Travis"
 
-  #using token clone gh-pages branch
-  git clone --quiet --branch=gh-pages https://d0a0fed90970b0afe84643ad5d5725b8a063743a@github.com/tcbeutler/TravisCITest.git > /dev/null
+  #using token clone master branch
+  git clone --quiet --branch=master https://d0a0fed90970b0afe84643ad5d5725b8a063743a@github.com/tcbeutler/TravisCITest.git > /dev/null
 
   #go into directory and copy data we're interested in to that directory
   cd coverage
