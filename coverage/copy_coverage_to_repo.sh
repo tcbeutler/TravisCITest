7a8de4aff150d44d2778c40686126c5612ab9497
tcbeutler <tcbeutler@gmail.com>
2013-11-13 00:06:30 -0600

    using both coverage types

--- coverage/copy_coverage_to_repo.sh
+++ coverage/copy_coverage_to_repo.sh
@@ -17,7 +17,32 @@ if [ "$TRAVIS_PULL_REQUEST" == "false" ]; then
   #[ci skip] to prevent constant build-commit-build loop.
   git add -f .
   git commit -m "Code coverage results from Travis build $TRAVIS_BUILD_NUMBER pushed to git repository. [ci skip]"
-  git push -fq origin gh-pages > /dev/null
+  git push -fq origin master > /dev/null
 
   echo -e "Code coverage results moved to git.\n"
+fi
+
+#same thing for github public pages
+if [ "$TRAVIS_PULL_REQUEST" == "false" ]; then
+  echo -e "Pushing code coverage data to git public pages.\n"
+
+  #copy data we're interested in to other place
+  cp -r coverage $HOME/coverage
+
+  git clone --quiet --branch=gh-pages https://2010976e1624b3640941858b87501f4560066125@github.com/tcbeutler/TravisCITest.git coverage_checkout > /dev/null
+
+  #go into directory and copy data we're interested in to that directory
+  cd coverage_checkout
+  cp -rf $HOME/coverage/* ./coverage
+
+  git config --global user.email "travis@travis-ci.org"
+  git config --global user.name "Travis"
+
+  #add, commit and push files
+  #[ci skip] to prevent constant build-commit-build loop.
+  git add -f .
+  git commit -m "Code coverage results from Travis build $TRAVIS_BUILD_NUMBER pushed to git repository. [ci skip]"
+  git push -fq origin gh-pages > /dev/null
+
+  echo -e "Code coverage results moved to git public pages.\n"
 fi
