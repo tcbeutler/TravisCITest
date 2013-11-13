b31c97a12331d49af49148b02ed6d496eec5ed4a
tcbeutler <tcbeutler@gmail.com>
2013-11-12 21:32:38 -0600

    take1 - travis coverage -> git

---
+++ coverage/copy_coverage_torepo.sh
@@ -0,0 +1,25 @@
+if [ "$TRAVIS_PULL_REQUEST" == "false" ]; then
+  echo -e "Pushing code coverage data to git.\n"
+
+  #copy data we're interested in to other place
+  cp -r coverage $HOME/coverage
+
+  #go to home and setup git
+  cd $HOME
+  git config --global user.email "travis@travis-ci.org"
+  git config --global user.name "Travis"
+
+  #using token clone gh-pages branch
+  git clone --quiet --branch=gh-pages https://d0a0fed90970b0afe84643ad5d5725b8a063743a@github.com/tcbeutler/TravisCITest.git > /dev/null
+
+  #go into directory and copy data we're interested in to that directory
+  cd coverage
+  cp -rf $HOME/coverage/* .
+
+  #add, commit and push files
+  git add -f .
+  git commit -m "Code coverage results from Travis build $TRAVIS_BUILD_NUMBER pushed to git repository."
+  git push -fq origin master > /dev/null
+
+  echo -e "Code coverage results moved to git.\n"
+fi
