3264144fa0cc8e81462e340f09790d2f1fb19c9a
tcbeutler <tcbeutler@gmail.com>
2013-11-13 00:22:59 -0600

    separated scripts for pages and regular stats

--- coverage/copy_coverage_to_repo_pages.sh
+++ coverage/copy_coverage_to_repo_pages.sh
@@ -1,15 +1,14 @@
 if [ "$TRAVIS_PULL_REQUEST" == "false" ]; then
-  echo -e "Pushing code coverage data to git.\n"
+  echo -e "Pushing code coverage data to git public pages.\n"
 
   #copy data we're interested in to other place
-  cp -r coverage $HOME/coverage
+  cp -r coverage/lcov-report $HOME/coverage
 
-  git clone --quiet --branch=master https://2010976e1624b3640941858b87501f4560066125@github.com/tcbeutler/TravisCITest.git coverage_checkout > /dev/null
+  git clone --quiet --branch=gh-pages https://2010976e1624b3640941858b87501f4560066125@github.com/tcbeutler/TravisCITest.git coverage_checkout_pages > /dev/null
 
   #go into directory and copy data we're interested in to that directory
-  cd coverage_checkout
-  mkdir ./coverage
-  cp -rf $HOME/coverage/* ./coverage
+  cd coverage_checkout_pages
+  cp -rf $HOME/coverage/* .
 
   git config --global user.email "travis@travis-ci.org"
   git config --global user.name "Travis"
@@ -18,8 +17,7 @@ if [ "$TRAVIS_PULL_REQUEST" == "false" ]; then
   #[ci skip] to prevent constant build-commit-build loop.
   git add -f .
   git commit -m "Code coverage results from Travis build $TRAVIS_BUILD_NUMBER pushed to git repository. [ci skip]"
-  git push -fq origin master > /dev/null
+  git push -fq origin gh-pages > /dev/null
 
-  echo -e "Code coverage results moved to git.\n"
+  echo -e "Code coverage results moved to git public pages.\n"
 fi
-
