ff11475ed33fde590432f42c3b12855dcec2dda1
tcbeutler <tcbeutler@gmail.com>
2013-11-12 22:01:09 -0600

    take 7, code coverage from travis

--- coverage/copy_coverage_to_repo.sh
+++ coverage/copy_coverage_to_repo.sh
@@ -1,20 +1,14 @@
 if [ "$TRAVIS_PULL_REQUEST" == "false" ]; then
   echo -e "Pushing code coverage data to git.\n"
 
-  #copy data we're interested in to other place
-  cp -r coverage $HOME/coverage
-
-  #go to home and setup git
-  cd $HOME
-  git config --global user.email "travis@travis-ci.org"
-  git config --global user.name "Travis"
-
-  #using token clone master branch
-  git clone --quiet --branch=master https://d0a0fed90970b0afe84643ad5d5725b8a063743a@github.com/tcbeutler/TravisCITest.git > /dev/null
+  git clone --quiet --branch=master https://2010976e1624b3640941858b87501f4560066125@github.com/tcbeutler/TravisCITest.git coverage_checkout > /dev/null
 
   #go into directory and copy data we're interested in to that directory
-  cd coverage
+  cd coverage_checkout
   cp -rf $HOME/coverage/* .
+
+  git config --global user.email "travis@travis-ci.org"
+  git config --global user.name "Travis"
 
   #add, commit and push files
   git add -f .
